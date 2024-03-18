e527a21da1c419223e096201f3ee96f1ab494857
Bodeode1 <bodeode2000@yahoo.com>
2024-03-18 05:13:08 +0500

    updated multiple prediction function and added styling

--- src/multiple_images_styles.py
+++ src/multiple_images_styles.py
@@ -4,23 +4,28 @@ def set_multiple_images_style():
 
     st.markdown("""
         <style>
-        .prediction-text {
-            font-size: 20px;
-            color: #FF4B4B;
+          .big-font {
+            font-size:25px !important;
             font-weight: bold;
+            color: #4CAF50;
             text-align: center;
+            margin-left: 52px
+        }
+        .image-display {
+            display: flex;
+            justify-content: center;
         }
         .stButton>button {
             font-size: 20px;
-            width: 200px;
-            height: 50px;
+            width: 200px; 
+            height: 50px;  
             border: 2px solid #4CAF50;
             border-radius: 25px;
-            margin: 10px 0;
+            margin: 10px 0;  
             display: block;
             margin-left: auto;
             margin-right: auto;
+            margin-left: 266px;
         }
-        /* Additional styles if required */
         </style>
         """, unsafe_allow_html=True)
