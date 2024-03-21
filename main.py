8eca858541e5fd0aae632fd9411ef1cc00a60749
Bodeode1 <121903724+Bodeode1@users.noreply.github.com>
2024-03-22 01:02:18 +0500

    Update add galley prediction

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import streamlit as st
-from app_pages import single_image, multiple_images, about
+from app_pages import single_image, multiple_images, about, image_gallery
 
-st.set_page_config(page_title="Cherry Leaf Disease Detection", layout="wide")
+st.set_page_config(page_title="Cherry Leaf Disease Detection", layout="centered")
 st.sidebar.title('Navigation')
-page = st.sidebar.radio('Go to', ['Single Image Prediction', 'Multiple Images Prediction', 'About'])
+page = st.sidebar.radio('Go to', ['Single Image Prediction', 'Multiple Images Prediction', 'Image Gallery Prediction', 'About'])
 
 if page == 'Single Image Prediction':
     single_image.show()
 elif page == 'Multiple Images Prediction':
     multiple_images.show()
+elif page == 'Image Gallery Prediction':
+    image_gallery.show()
 elif page == 'About':
     about.show()
