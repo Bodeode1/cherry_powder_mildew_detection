fa7fe3f9b5e5053657d315ee2eff847f18dc0bab
Bodeode1 <121903724+Bodeode1@users.noreply.github.com>
2024-03-22 23:42:17 +0500

    Added Hypothesis and validation

--- app_pages/about.py
+++ app_pages/about.py
@@ -5,16 +5,39 @@ def show():
 
     st.markdown("""
             ## 🌿 Powdery Mildew in Cherry Plants
-            Powdery mildew is a common fungal disease that affects a wide variety of plants. It appears as white or gray powdery spots on the leaves and stems of plants. 🍃
+            Powdery mildew is a common fungal disease that affects a wide variety of plants. It appears as white or gray powdery spots on the leaves and stems of plants. 🍃 So, Effective treatment involves the use of fungicides and adopting proper gardening practices to prevent the spread of the disease. 🌱
      """)
 
     st.image('images/mildew.png', caption='Powdery Mildew on Cherry Leaves', use_column_width=True)
 
+   st.markdown("""
+           ## Project Overview
+
+           - **Data Acquisition**: The cherry leaves dataset, comprising images categorized into healthy and powdery mildew-infected leaves, was fetched from Kaggle.
+
+           - **Data Preparation**: Employed image preprocessing and augmentation techniques, such as rotation, shifting, and flipping, to enhance model training.
+
+           - **Exploratory Data Analysis**: Conducted visualizations to understand the dataset's class distribution and the average color distribution among classes.
+
+           - **Model Development**: Developed a CNN model, tuned its hyperparameters, and evaluated its performance using metrics such as accuracy, precision, and recall.
+
+           - **Model Deployment**: The trained model was saved and integrated into this web application for real-time disease detection.
+       """)
     st.markdown("""
-         Effective treatment involves the use of fungicides and proper gardening practices to prevent the spread of the disease. 🌱
+           ## Hypotheses and Validation
 
-            The model is designed to detect powdery mildew in cherry plants using images of the leaves. By leveraging deep learning techniques, it can accurately distinguish between healthy leaves 🍒 and those affected by the disease. 🍂
+           - **Hypothesis 1**: The machine learning model can accurately detect powdery mildew in cherry leaves through image analysis.
 
-        """)
+             **Validation**: The model's performance was evaluated using precision, recall, and F1-score on a designated test dataset. The validation criterion was set at a minimum accuracy of 90%.
 
+           - **Hypothesis 2**: Identification of powdery mildew can be enhanced by analyzing the color and textural characteristics of cherry leaves.
+
+             **Validation**: Utilized visualization plots to examine the primary color distributions and textural features that the model relies on for making predictions.
+       """)
+
+ st.markdown("""
+           
+
+           ## Model Archietecture
+       """)
     st.image('images/diagram.png', caption='Model Architecture', use_column_width=True)
