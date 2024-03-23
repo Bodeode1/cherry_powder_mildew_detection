2ee84b2785e38539ff6c29da090a6ab2c464d310
Bodeode1 <121903724+Bodeode1@users.noreply.github.com>
2024-03-23 16:05:40 +0500

    added performace page

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from app_pages import single_image, multiple_images, about, image_gallery
 
 st.set_page_config(page_title="Cherry Leaf Disease Detection", layout="centered")
 st.sidebar.title('Navigation')
-page = st.sidebar.radio('Go to', ['Single Image Prediction', 'Multiple Images Prediction', 'Image Gallery Prediction', 'About'])
+page = st.sidebar.radio('Go to', ['Single Image Prediction', 'Multiple Images Prediction', 'Image Gallery Prediction', 'About Page','Model Performance Page'])
 
 if page == 'Single Image Prediction':
     single_image.show()
@@ -11,5 +11,7 @@ elif page == 'Multiple Images Prediction':
     multiple_images.show()
 elif page == 'Image Gallery Prediction':
     image_gallery.show()
-elif page == 'About':
+elif page == 'About Page':
     about.show()
+elif page == 'Model Performance Page':
+    performance.show()
